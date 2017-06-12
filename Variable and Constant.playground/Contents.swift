ddfe5a1d635a62452aa5a8d54fbf7cf7bfc18f33
sutisa <a_sutisa@snru.ac.th>
2017-06-12 16:14:06 +0700

    variable

--- Variable and Constant.playground/Contents.swift
+++ Variable and Constant.playground/Contents.swift
@@ -16,3 +16,46 @@ let myConstant1: Int = 123
 print(intNumber) //display only
 
 print("intNumber ===> \(intNumber)") //display string and value
+
+// explicit String การประกาศสตริง แบบกำหนด
+// Data type ==> String
+let Strname: String = "Doramon"
+
+let StrSurname = "japan"
+
+//operate
+Strname + "  " + StrSurname
+var office = Strname + "   " + StrSurname
+print (office)
+
+// operating on number
+let intA = 5
+let intB = 2
+var answer = intA + intB
+answer = intA * intB
+
+// chang datatype int to double
+var douA = Double(intA)
+var douB = Double(intB)
+var answer2: Double = douA / douB
+
+var myStatus = true
+var myStatus2: Bool = false
+
+// หากเอา 5 บวก 6  เท่ากับ 11
+print("5+6 ===>", 5+6, 5/6)
+let strTitle1 = "ถ้าเอา"
+let strTitle2 = "เท่ากับ"
+let strAdd = "บวก"
+let strHand = "หาร"
+let strSpace = " "
+
+let intA1 = 5
+let intB1 = 6
+
+print(strTitle1 + strSpace + "\(intA1)" + strSpace + strAdd + strSpace + "\(intB1)" + strSpace + strTitle2 + strSpace + "\(intA1+intB1)")
+print(strTitle1 + strSpace + "\(intA1)" + strSpace + strHand + strSpace + "\(intB1)" + strSpace + strTitle2 + strSpace + "\(Double(intA1)/Double(intB1))")
+
+
+
+
